9a94d42f22a9f4fb9ee8187592ed67db16e6a6fa
basementlivin <epedersen13@gmail.com>
2022-08-10 09:44:22 -0400

    Add a CLICK event listener

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// const sheetMusic = document.getElementsByClassName("sheet-music");
+// const performance = document.getElementsByClassName("performance-prompt");
+
+document.addEventListener("click", countClicks);
+
+function countClicks(){
+    console.log("YOU'RE ONTO SOMETHING, PAL")
+}
