2182a8abea783346e3056bf6acaa289cebd49d43
basementlivin <epedersen13@gmail.com>
2022-08-10 19:44:04 -0400

    Attempt to write calculateKeyDownTime function

--- scripts.js
+++ scripts.js
@@ -9,25 +9,28 @@ const prompts = [
     "id": "prompt-2",
     "domEvent": "keydown",
     "amount": 3,
+    "keyCode": 32, // todo: require the keyCode to match the prompt; right now, users can press any key and the function will work & proceed to the next prompt.
     "text": "Hit the spacebar 3 times."
   },
   {
     "id": "prompt-3",
     "domEvent": "click",
-    "amount": 15,
-    "text": "Click the left mouse button 15 times."
+    "amount": 7,
+    "text": "Click the left mouse button 7 times."
   },
   {
     "id": "prompt-4",
     "domEvent": "keydown",
     "amount": 3,
-    "text": "Hit the spacebar 3 times."
+    "keyCode": 49,
+    "text": "Hit the 1 key 3 times."
   },
   {
     "id": "prompt-5",
-    "domEvent": "keydown",
-    "amount": 3,
-    "text": "Press and hold the shift key for 7 seconds."
+    "domEvent": "keyhold",
+    "time": Date.now,  // could the function start counting from the user's time on keyDown?
+    "duration": 7000,
+    "text": "Press and hold the G key for 7 seconds."
   }
 ]
 
@@ -39,11 +42,15 @@ document.getElementById(domId).innerHTML = newText;
 }
 
 function updatePrompt(){
-const newPromptIndex = promptIndex + 1
-promptIndex = newPromptIndex 
-const prompt = prompts[newPromptIndex] 
-console.log("UPDATED PROMPT IS:", JSON.stringify(prompt)) 
-updateTheDom("performance-prompt", prompt.text) 
+  const newPromptIndex = promptIndex + 1
+  if (newPromptIndex > (prompts.length - 1)) {
+    console.log("oh no! we already did all the prompts! what should we do now?!")
+  } else {
+    promptIndex = newPromptIndex
+    const prompt = prompts[newPromptIndex] 
+    console.log("UPDATED PROMPT IS:", JSON.stringify(prompt)) 
+    updateTheDom("performance-prompt", prompt.text) 
+  }
 }
 
 
@@ -86,5 +93,27 @@ function keyDownHandler(){
   const currentPrompt = prompts[promptIndex];
   if (currentPrompt["domEvent"] === "keydown") {
     countKeyDowns(currentPrompt)
+  }
+}
+
+
+// CALCULATE TIME BETWEEN KEYDOWN AND KEYUP EVENTS
+
+document.addEventListener("keyhold", keyHoldHandler)
+
+let keyDownTime = 0;
+
+function calculateKeyDownTime(currentPrompt){
+  keyDownTime = keyDownTime + 1
+  if (keyDownTime === currentPrompt["time" + "duration"]){
+    keyDownTime = 0
+    updatePrompt();
+  }
+}
+
+function keyHoldHandler(){
+  const currentPrompt = prompts[promptIndex];
+  if (currentPrompt["domEvent"] === "keyhold") {
+    calculateKeyDownTime(currentPrompt)
   }
 }
