b1854d60fd80061d27a73a2a5909c96d3e0414d7
basementlivin <epedersen13@gmail.com>
2022-08-12 01:19:45 -0400

    Improve CSS positioning; edit prompts[]

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
 const prompts = [
   {
     "id": "prompt-1",
-    "domEvent": "click",
+    "domEvent": "keydown",
     "amount": 3,
-    "text": "Click the left mouse button 3 times."
+    "keyCode": "KeyB",
+    "text": "Lightly tap the B key 3 times."
   },
   {
     "id": "prompt-2",
     "domEvent": "keydown",
-    "amount": 3,
-    "keyCode": "KeyH" || 72,
-    "text": "Tap the H key 3 times."
+    "amount": 10,
+    "keyCode": "KeyS",
+    "text": "Hit the S key 10 times."
   },
   {
     "id": "prompt-3",
@@ -21,23 +22,57 @@ const prompts = [
   {
     "id": "prompt-4",
     "domEvent": "keydown",
-    "amount": 3,
-    "keyCode": "Digit1" || 49,
-    "text": "Hit the 1 key 3 times."
+    "amount": 20,
+    "keyCode": "Digit1",
+    "text": "Hit the 1 key 20 times."
   },
   {
     "id": "prompt-5",
     "domEvent": "keyhold",
     "duration": 4000,
-    "keyCode": "KeyG",
-    "text": "Press and hold the G key for 4 seconds, then lift."
+    "keyCode": "Space",
+    "text": "Press and hold the spacebar for at least 4 seconds, then lift."
   },
   {
     "id": "prompt-6",
     "domEvent": "click",
-    "amount": 40,
-    "text": "Click the left mouse button 40 times."
-  }
+    "amount": 1,
+    "text": "Left click once."
+  },
+  {
+    "id": "prompt-7",
+    "domEvent": "keyhold",
+    "duration": 10000,
+    "keyCode": "KeyG",
+    "text": "Press and hold the G key for 10 seconds, then lift."
+  },
+  {
+    "id": "prompt-8",
+    "domEvent": "click",
+    "amount": 1,
+    "text": "Left click once."
+  },
+  {
+    "id": "prompt-9",
+    "domEvent": "keyhold",
+    "duration": 30000,
+    "keyCode": "Digit6",
+    "text": "Press and hold the 6 key for 30 seconds, then lift."
+  },
+  {
+    "id": "prompt-10",
+    "domEvent": "keyhold",
+    "duration": 4000,
+    "keyCode": "Space",
+    "text": "Press and hold the G key for 4 seconds, then lift."
+  },
+  {
+    "id": "prompt-11",
+    "domEvent": "keyhold",
+    "duration": 4000,
+    "keyCode": "Space",
+    "text": "Press and hold the G key for 4 seconds, then lift."
+  },
 ]
 
 let promptIndex = 0;
